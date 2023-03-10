bac79ef7b59983dd003688001667ad3820942347
孤灯灵 <2809551464@qq.com>
2023-03-10 21:17:42 +0800

    ✨ feat: 完成基本功能

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { json } from "stream/consumers";
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "runner" is now active!');
@@ -13,10 +13,36 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('runner.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand("runner.run", () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from runner!');
+		// vscode.window.showInformationMessage("Hello World from runner!");
+		// TODO 获取配置
+		const config = vscode.workspace.getConfiguration("runner");
+		let message = JSON.stringify(config.get("custom_command"));
+		let commands = JSON.parse(message);
+		let languageId = vscode.window.activeTextEditor?.document.languageId || "";
+		let command = commands[languageId] || "";
+
+		if (!command) {
+			return vscode.window.showErrorMessage(`No command for ${languageId} !`);
+		}
+
+		// TODO 获取当前文件的文件夹路径
+		let path = vscode.window.activeTextEditor?.document.fileName;
+		let dir = path?.substring(0, path.lastIndexOf("\\"));
+		let file = path?.substring(path.lastIndexOf("\\") + 1);
+		// vscode.window.showInformationMessage(dir || "");
+
+		// TODO 打开terminal, 并切换到当前文件的文件夹路径
+		// 判断当前是否有terminal Runner
+		let terminals = vscode.window.terminals;
+		let shell =
+			terminals.find((item) => item.name === "Runner") ||
+			vscode.window.createTerminal("Runner");
+		shell.show();
+		shell.sendText(`cd ${dir}`);
+		shell.sendText(`${command} ${file}`);
 	});
 
 	context.subscriptions.push(disposable);
